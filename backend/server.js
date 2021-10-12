1314fa29ba6a9f5afd0e31676a24de167c8c06e2
Badmus Adeshina <badmusadeshinna@gmail.com>
2021-10-12 22:39:58 +0100

    proxyissuesorted

--- backend/server.js
+++ backend/server.js
@@ -1,21 +1,27 @@
-import express from 'express'
-import dotenv from 'dotenv'
-import colors from 'colors'
-import connectDB from './config/db.js'
-import productRoutes from './routes/productRoutes.js'
+import express from 'express';
+import dotenv from 'dotenv';
+import colors from 'colors';
+import connectDB from './config/db.js';
+import productRoutes from './routes/productRoutes.js';
 
-dotenv.config()
+dotenv.config();
 
-connectDB()
+connectDB();
 
-const app = express()
+const app = express();
 
-app.get('/', (req,res) =>{
-    res.send('API is running...')
-})
+app.get('/', (req, res) => {
+  res.send('API is running...');
+});
 
-app.use('/api/products' , productRoutes)
+app.use('/api/products', productRoutes);
 
-const PORT = process.env.PORT || 5000
+const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, console.log(`Server running in ${process.env.NODE_ENV} mode on a port ${PORT}` .yellow.bold))
+app.listen(
+  PORT,
+  console.log(
+    `Server running in ${process.env.NODE_ENV} mode on a port ${PORT}`.yellow
+      .bold
+  )
+);
